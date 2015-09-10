f4add80359795628371ad3c59d5ae2e9ece9f872
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-10 14:04:40 -0500

    barebones

---
+++ app/models/CalendarEvent.php
@@ -0,0 +1,8 @@
+<?php
+
+use \Esensi\Model\Model;
+
+class CalendarEvent extends Model {
+	protected $fillable = [];
+	protected $table = 'calendar_events';
+}
---
+++ env-template.php
@@ -0,0 +1,10 @@
+<?php
+
+	return [
+		'DB_HOST' => '',
+		'DB_NAME' => '',
+		'DB_USER' => 'r',
+		'DB_PASS' => ''
+	];
+
+?>
