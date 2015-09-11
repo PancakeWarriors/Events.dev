2c3981e30b56d1dd0ecdc00db3f8f2c49fb7b889
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-11 15:09:07 -0500

    added calendar event controller and users controller, added tag model, implement them in tables later

--- app/models/CalendarEvent.php
+++ app/models/CalendarEvent.php
@@ -14,6 +14,11 @@ class CalendarEvent extends Model {
 	{
 	    return $this->belongsTo('User');
 	}
+
+	public function tags()
+	{
+		return $this->belongsToMany('Tag', 'calendar_event_tag')->withTimestamps();
+	}
 	
 	protected $rules = array(
 		'start_dateTime' => 'required|max:255',
@@ -22,4 +27,36 @@ class CalendarEvent extends Model {
 		'description' => 'required|max:255',
 		'price' => 'required|max:255',
 	);
+
+	public static function urlBuilder()
+	{
+		// $getRequests = Input::except('p');
+		// if(isset($getRequests['user'])){
+		// 	return "?user=" . $getRequests['user'];
+		// }elseif(isset($getRequests['tag'])){
+		// 	return "?tag=" . $getRequests['tag'];
+		// }elseif(isset($getRequests['search'])){
+		// 	return "?search=" . $getRequests['search'];
+		}
+	}
+
+	public static function findTag($tag)
+	{
+		$query = Tag::where('name', '=', $tag)->get();	
+		return $query;
+	}
+
+	public static function storeTags($tag,$post)
+	{
+		if(CalendarEvent::findTag($tag)->first()){
+			$tags = Tag::where('name', '=' , $tag);
+			$post->tags()->attach($tags->first()->id);
+		}else{
+			$tags = new Tag();
+			$tags->name = $tag;
+			$post->tags()->save($tags);
+		}
+	}
+
+
 }
