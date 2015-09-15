cde390a87161fb8814e6873880cf82aa8146a241
asteph <alissa.stephens1@gmail.com>
2015-09-15 13:22:49 -0500

    corrected paths for calendar; most simple view is now displayed on user.calendar TODO: add buttons to switch between months, and figure out how to populate calendar

--- app/views/users/calendar.blade.php
+++ app/views/users/calendar.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.master')
-
+@section('head')
+	<link rel="stylesheet" href="/vendor/bootstrap-calendar/css/calendar.css">
+@stop
 @section('content')
 {{-- /vagrant/sites/events.dev/app/views/users/calendar.blade.php --}}
+	<header id="head" class="secondary"></header>
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb">
+			<li><a href="{{{ action('HomeController@showHome')}}}">Home</a></li>
+			<li class="active">User Profile</li>
+		</ol>
+
+		<div class="row">
+			{{-- show message --}}
+			@if (Session::has('message'))
+			    <div class="alert alert-success">{{{ Session::get('message') }}}</div>
+			@endif
+			<!-- Profile main content -->
+			<article class="col-xs-12 maincontent">
+				<header class="page-header">
+					<h1 class="page-title">{{ $user->first_name }} {{ $user->last_name }}</h1>
+				</header>
+				<div id="calendar"></div>
+
+			</article>
+			<!-- /Article -->
+
+		</div>
+	</div>	<!-- /container -->
+@stop
+@section('script')
+    <script type="text/javascript" src="/vendor/underscore/underscore-min.js"></script>
+    <script type="text/javascript" src="/vendor/bootstrap-calendar/js/calendar.js"></script>
+    <script type="text/javascript">
+        var calendar = $("#calendar").calendar(
+            {
+                tmpl_path: "/vendor/bootstrap-calendar/tmpls/",
+                events_source: function () { return []; }
+            });         
+    </script>
 @stop
