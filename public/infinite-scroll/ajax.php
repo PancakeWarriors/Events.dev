4e0fa636e1af2bbb9057b08add054a775bfae971
zakkyzebra <zzaacchhyy010@yahoo.com>
2015-09-14 23:09:59 -0500

    updated the create events page a bit, worked on infinite scroll, got it to work but its kind of buggy when trying to make it more user friendly

---
+++ public/infinite-scroll/ajax.php
@@ -0,0 +1,22 @@
+<?php
+	require_once('connect.php');
+	$load = htmlentities(strip_tags($_POST['load'])) * 5;
+	$query = $connect->query("SELECT * FROM calendar_events ORDER BY updated_at DESC LIMIT ".$load.",5");
+	
+	while($row = $query->fetch())
+	{
+		?>
+			<div class="col-md-12">
+				<h3>
+					<a href="{{{ action('CalendarEventsController@show', <?php echo $row['id'];?>}}}"><?php echo $row['title']; ?>
+				</h3>
+				<p><img src="http://lorempixel.com/400/400" alt="" class="img-rounded pull-left" width="300" height="200" ><?php echo $row['description'];?></p>
+				<h5>From: <?php echo date_create($row['start_dateTime'])->format('l, F jS Y @ h:i:s a');?></h5>
+				<h5>To: <?php echo date_create($row['end_dateTime'])->format('l, F jS Y @ h:i:s a')?></h5></a>
+
+			</div>
+		<?php
+	}
+
+
+?>
